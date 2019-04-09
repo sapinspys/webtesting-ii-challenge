0fa305394a389a09cee5580324843886a937daca
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-09 17:28:50 -0400

    Action types created

---
+++ client/src/actions/index.js
@@ -0,0 +1,8 @@
+export const STRIKE_REQUESTED = "STRIKE_REQUESTED"
+export const BALL_REQUESTED = "BALL_REQUESTED"
+export const FOUL_REQUESTED = "FOUL_REQUESTED"
+export const HIT_REQUESTED = "HIT_REQUESTED"
+export const STRIKE = "STRIKE"
+export const BALL = "BALL"
+export const FOUL = "FOUL"
+export const HIT = "HIT"
