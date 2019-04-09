628a4ddad709bb8c4687bf757c6626aed8dd27bb
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-09 18:32:24 -0400

    Tests are successful

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+// MUST PASS IN APP AS BELOW BECAUSE OF REDUX STORE
+import { App } from './App';
+
+import { render } from 'react-testing-library'; 
+import 'jest-dom/extend-expect';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(App, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('renders successfully', () => {
-    render(<App />);
+    render(App);
   });
 })
